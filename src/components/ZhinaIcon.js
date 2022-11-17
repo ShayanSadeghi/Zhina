44d486202b45444278e9e2ed485a1eea1d86265c
Shayan <ShayanSadeghi1996@gmail.com>
2022-11-17 20:46:43 +0330

    added icon and animation

--- src/components/ZhinaIcon.js
+++ src/components/ZhinaIcon.js
@@ -1,7 +1,7 @@
 import image from "../MahsaAmini.jpg";
 import styles from "../styles/ZhinaIcon.module.css";
 
-export default function ZhinaIcon({ size }) {
+export default function ZhinaIcon({ size, withText }) {
   let image_style = "image_";
   if (["small", "medium", "large"].includes(size)) {
     image_style += size;
@@ -12,6 +12,9 @@ export default function ZhinaIcon({ size }) {
   return (
     <div className={styles.container}>
       <img src={image} className={styles[image_style]} alt="MahsaAmini" />
+      <div className={styles.box}>
+        <span>{withText ? "#MahsaAmini" : ""}</span>
+      </div>
     </div>
   );
 }
